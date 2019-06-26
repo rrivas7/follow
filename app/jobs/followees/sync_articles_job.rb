48a1f7b7a3a477634b333006de94fa68642eb005
Renzo Rivas <renzo.rivas@smiledirectclub.com>
2019-06-25 22:19:20 -0500

    Updating README

--- app/jobs/followees/sync_articles_job.rb
+++ app/jobs/followees/sync_articles_job.rb
@@ -2,9 +2,11 @@ module Followees
   class SyncArticlesJob < ::ApplicationJob
     queue_as :default
 
+    JOB_FREQUENCY = 10.minutes
+
     def perform
-      from_datetime = ::DateTime.current
-      to_datetime = from_datetime - 1.minute
+      to_datetime = ::DateTime.current
+      from_datetime = to_datetime - JOB_FREQUENCY
       followees.each do |followee|
         SyncArticlesService.new(followee: followee).sync_articles(
           from_datetime: from_datetime.to_s,
--- app/services/news_api_service.rb
+++ app/services/news_api_service.rb
@@ -2,7 +2,7 @@ class NewsApiService
   GET_EVERYTHING_ENDPOINT = 'https://newsapi.org/v2/everything'.freeze
 
   def get_everything(params: {})
-    query = params.merge(apiKey: Follow::Application.credentials.NEWS_API_KEY)
+    query = params.merge(apiKey: Follow::Application.credentials.news_api_key)
     ::HTTParty.get(GET_EVERYTHING_ENDPOINT, query: query)
   end
 end
